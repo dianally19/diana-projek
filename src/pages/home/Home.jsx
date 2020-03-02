21f05d95b01b275343ab26cef528ba63b23815d6
unknown <user@NB-DIANA.ag-it.com>
2020-03-02 12:02:00 +0700

    new message

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import Axios from 'axios';
-import { urlApi } from '../../helper/database'
-
-
-// import Photo1 from '../../photo/logo.jpg'
-// import Photo2 from '../../photo/img1.jpg'
-
-export default class Home extends Component {
-    
-    getData = () => {
-        Axios.get(urlApi + 'customer/getTest')
-        .then((res) => {
-            console.log(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }
-
-    componentDidMount() {
-        this.getData()
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>HOME</h1>
-                <Link to='/signup'>
-                    <button className='btn btn-light'>Sign Up</button>
-                </Link>
-            </div>
-        )
-    }
-}
-
-=======
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import Axios from 'axios';
@@ -73,4 +34,3 @@ export default class Home extends Component {
     }
 }
 
->>>>>>> commit
--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-import { urlApi } from '../../helper/database'
-
-export const loginUser = (emailUser) => {
-    return (dispatch) => {
-         axios.get(urlApi + 'customer/getDataLogin/' + emailUser)
-         .then((res) => {
-             if(res.data.length > 0) {
-                 dispatch({
-                     type: 'LOGIN',
-                     payload: {
-                        id: res.data[0].id,
-                        username: res.data[0].nama,
-                        password: res.data[0].password,
-                        role: res.data[0].role,
-                        email: res.data[0].email
-                     }
-                 })
-                 alert(`Welcome, ${res.data[0].nama} !`)
-             } else {
-                 alert('Username salah!')
-             }
-         })
-         .catch((err) => {
-             alert('Data gak dapet bro!')
-             console.log(err)
-         })
-    }
-=======
 import axios from 'axios'
 import { urlApi } from '../../helper/database'
 
@@ -67,5 +37,4 @@ export const logOut = () => {
             alert('anda telah keluar')
         }
     }
->>>>>>> commit
 }
